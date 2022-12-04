f4a3675fa8e0bdc0212073302de77b9a23c6f20e
DylanMacres <Dylan.Macres@du.edu>
2022-12-03 17:14:17 -0700

    adding server boiler plate

---
+++ server/schema/typedefs.js
@@ -0,0 +1,21 @@
+
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    name: String
+    username: String
+    email: String
+    password: String
+}
+type Auth {
+    token: String
+    user: User
+}
+type Query {
+    me: User
+}
+`;
+
+module.exports = typeDefs;
