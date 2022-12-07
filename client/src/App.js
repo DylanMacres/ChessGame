3139c000a438bbcd181f2767323704e6c20d5a4a
DylanMacres <Dylan.Macres@du.edu>
2022-12-06 20:18:09 -0700

    trying to make it work on heroku

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import socketIO from "socket.io-client"
 import { Chessboard } from 'react-chessboard';
 import './App.css';
 import '../src/assets/login.css'
-// import LogIn from "./components/loginpage";
+import LogIn from "./components/loginpage";
 // import jQuery from "jquery";
 import {
   ApolloClient,
@@ -50,7 +50,7 @@ function App() {
          
             <Route path="/chess" element={<Chessboard className = "board"/>}></Route>
             <Route path="/" element={<Home socket={socket}/>}></Route>
-            {/* <Route path="/login" element={<LogIn socket={socket} jQuery/>}></Route> */}
+            <Route path="/login" element={<LogIn socket={socket} jQuery/>}></Route>
             <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
           </Routes>
     </div>
--- client/src/components/loginpage.js
+++ client/src/components/loginpage.js
@@ -1,80 +1,80 @@
-// import React, {useState} from 'react'
-// import {useNavigate} from "react-router-dom"
+import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom"
 
 
 
-// const LogIn = ({socket}) => {
-//     const navigate = useNavigate()
-//     const [userName, setUserName] = useState("")
+const LogIn = ({socket}) => {
+    const navigate = useNavigate()
+    const [userName, setUserName] = useState("")
 
-//     const handleSubmit = (e) => {
-//         e.preventDefault()
-//         localStorage.setItem("userName", userName)
-//         socket.emit("newUser", {userName, socketID: socket.id})
-//         navigate("/chat")
-//     }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        localStorage.setItem("userName", userName)
+        socket.emit("newUser", {userName, socketID: socket.id})
+        navigate("/chat")
+    }
 
-// return(
+return(
 
-//   <div className="container">
-//     <div className="row px-3">
-//       <div className="col-lg-10 col-xl-9 card flex-row mx-auto px-0">
-//         <div className="img-left d-none d-md-flex">
-//           <div className="card-body">
-//             <h4 className="title text-center mt-4"> Account Login</h4>
-//             <form className="form-box px-3"/>
-//               <div className="form-input">
+  <div className="container">
+    <div className="row px-3">
+      <div className="col-lg-10 col-xl-9 card flex-row mx-auto px-0">
+        <div className="img-left d-none d-md-flex">
+          <div className="card-body">
+            <h4 className="title text-center mt-4"> Account Login</h4>
+            <form className="form-box px-3"/>
+              <div className="form-input">
                 
-//                 <input type="email" name="" placeholder="📧  email@domain.com" required/>
+                <input type="email" name="" placeholder="📧  email@domain.com" required/>
   
-//               <div className="form-input">
+              <div className="form-input">
                
-//                 <input type="password" name="" placeholder="🔑  password" required/>
-//               </div>
+                <input type="password" name="" placeholder="🔑  password" required/>
+              </div>
 
-//               <div className="mb-3">
-//                 <div className="cutom-control custom-checkbox">
-//                 <button onClick={()=>navigate("/chat")} classNameName="button1">Click Me To Log In!</button>
-//                   <label className="custom-control-label" for="cb1"> Remember me</label>
-//                 </div>
-//               </div>
+              <div className="mb-3">
+                <div className="cutom-control custom-checkbox">
+                <button onClick={()=>navigate("/chat")} classNameName="button1">Click Me To Log In!</button>
+                  <label className="custom-control-label" for="cb1"> Remember me</label>
+                </div>
+              </div>
 
 
 
-//               <div className="mb-3">
-//                 {/* <button type="submit" className="btn btn-block text-uppercase">
-//                   Login
-//                 </button> */}
-//               </div>
+              <div className="mb-3">
+                {/* <button type="submit" className="btn btn-block text-uppercase">
+                  Login
+                </button> */}
+              </div>
 
-//               <div className="text-center mb-3">
-//                 <div className="col-4"><a href="#" className="btn btn-facebook">
-//                      facebook
-//                   </a> 
+              <div className="text-center mb-3">
+                <div className="col-4"><a href="#" className="btn btn-facebook">
+                     facebook
+                  </a> 
                  
-//                 </div>
+                </div>
 
-//                 <div className="col-4"><a href="#" className="btn btn-google">
-//                   Google
-//                </a> 
-//              </div>
-//              <hr className="my-4"/>
-//              <div className="text-center mb-2">
-//                 Don't have an account?
-//                 <a href="#" className="register-link">
-//                   Register here
-//                 </a>
-//              </div>
+                <div className="col-4"><a href="#" className="btn btn-google">
+                  Google
+               </a> 
+             </div>
+             <hr className="my-4"/>
+             <div className="text-center mb-2">
+                Don't have an account?
+                <a href="#" className="register-link">
+                  Register here
+                </a>
+             </div>
 
 
-//               </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-//   </div>
-//   )
-// }
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  )
+}
 
-// export default LogIn
+export default LogIn
