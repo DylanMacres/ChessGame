26579be6c93ae92b3a6c0bd5bf55346a0d9a9495
DylanMacres <Dylan.Macres@du.edu>
2022-12-06 18:08:37 -0700

    making the login page populate

--- client/src/components/ChatBody.js
+++ client/src/components/ChatBody.js
@@ -7,7 +7,7 @@ const ChatBody = ({messages, typingStatus, lastMessageRef}) => {
 
   const handleLeaveChat = () => {
     localStorage.removeItem("userName")
-    navigate("/")
+    navigate("/home")
     window.location.reload()
   }
   
